078542de277490980b3873547e064f5ae80e8858
Raja Sharma <rajasharma9180@gmail.com>
2026-09-09 09:19:47 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (45.00%) - LeetSync

---
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def countCommas(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        count = 0
+        if n >= 1000:
+            count += n - 999
+        if n >= 1000000:
+            count += n - 999999
+        if n >= 1000000000:
+            count += n - 999999999
+        if n >= 1000000000000:
+            count += n - 999999999999
+        if n >= 1000000000000000:
+            count += n - 999999999999999
+        return count
